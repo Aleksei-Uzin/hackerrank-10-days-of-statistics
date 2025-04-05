d4dc8ea5d2190edb9a34b8e7b0604b7fb709514f
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-04-05 14:32:54 +0300

    day 0 solution: Mean, Median, and Mode

---
+++ day-0a/processData.js
@@ -0,0 +1,62 @@
+function processData(input) {
+  const [_, str] = input.trim().split('\n')
+  const arr = str.split(' ').map(Number)
+
+  const calcMean = arr => {
+    const sum = arr.reduce((acc, n) => acc + n, 0)
+    const mean = sum / arr.length
+    return mean.toFixed(1)
+  }
+
+  const calcMedian = arr => {
+    const sorted = [...arr].sort((a, b) => a - b)
+    const len = arr.length
+    const mid = Math.floor(len / 2)
+    let median = null
+
+    if (len % 2 === 0) {
+      median = (sorted[mid] + sorted[mid - 1]) / 2
+    } else {
+      median = sorted[mid] / 2
+    }
+
+    return median.toFixed(1)
+  }
+
+  const calcMode = arr => {
+    const amount = {}
+    let max = 0
+    let mode = Infinity
+
+    for (let val of arr) {
+      if (Object.hasOwn(amount, val)) amount[val] += 1
+      else amount[val] = 1
+
+      if (amount[val] > max || (amount[val] === max && val < mode)) {
+        max = amount[val]
+        mode = val
+      }
+    }
+
+    return mode
+  }
+
+  const mean = calcMean(arr)
+  const median = calcMedian(arr)
+  const mode = calcMode(arr)
+
+  console.log(mean)
+  console.log(median)
+  console.log(mode)
+}
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+_input = ''
+process.stdin.on('data', function (input) {
+  _input += input
+})
+
+process.stdin.on('end', function () {
+  processData(_input)
+})
